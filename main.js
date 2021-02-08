e4af7da2b7f9794810518771f28dd34fefbeb34e
adele <adele.r87@gmail.com>
2021-02-08 12:53:17 +0100

    modifiche

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ var app = new Vue({
         dischi: [],
     },
     mounted() {
+      const self = this;
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((risposta) => {
-        this.dischi = risposta.data.response;
+        self.dischi = risposta.data.response;
   });
 
 
     }
 
 });
+
+Vue.config.devtools = true
