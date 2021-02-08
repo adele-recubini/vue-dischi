bb5d3133ba2c8381254c87385c53fa736d3b997d
adele <adele.r87@gmail.com>
2021-02-08 14:37:59 +0100

    modifiche

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
+// Bonus (per oggi pomeriggio): Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+
+
 var app = new Vue({
     el: "#root",
     data: {
         dischi: [],
+        generi:[],
+
     },
     mounted() {
-      const self = this;
+    const self = this;
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((risposta) => {
         self.dischi = risposta.data.response;
+        console.log(self.dischi);
+        // ora devo prendermi per ogni disco la chiave genere dal momento che ora tutto il mio data.response è in un normalissimo array in data procedo a estrarre le informazioni con un foreach
+        self.dischi.forEach((element) => {
+
+          if (!self.generi.includes(element.genre)){
+             self.generi.push(element.genre)
+
+
+
+          }
+
+        });
+
+
+
+
   });
 
 
